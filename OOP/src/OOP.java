e52551d19f9a026486b93137864af6aede15e73f
Patrickjoshanedez <153726960+Patrickjoshanedez@users.noreply.github.com>
2024-11-07 22:27:09 +0800

    Initial commit of project.

---
+++ OOP/src/OOP.java
@@ -0,0 +1,19 @@
+
+public class OOP {
+	String name;
+	int age;
+	public OOP(String name, int age) {
+	this.name = name;
+	this.age = age;
+	}
+	public static void main(String[] args) {
+		
+	OOP Clarise = new OOP("Clarise", 18);
+	OOP Bob = new OOP("Bob", 19);
+	System.out.println(Bob);
+	System.out.println(Clarise);
+		
+
+	}
+
+}
